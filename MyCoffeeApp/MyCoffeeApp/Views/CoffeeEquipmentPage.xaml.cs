f99a36732162866559f1cdef3ffe1fee780c065b
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2022-02-11 14:22:48 +0100

    Increment the number of buttonclicks and display it in a label

--- MyCoffeeApp/MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
+++ MyCoffeeApp/MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,6 +11,15 @@ namespace MyCoffeeApp.Views
         {
             InitializeComponent();
             LabelCount.Text = "Hello from Code Behind!";
+        }
+
+
+        int count = 0;
+
+        private void ButtonCLick_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            LabelCount.Text = $"You clicked {count} time(s).";
         }
     }
 }
