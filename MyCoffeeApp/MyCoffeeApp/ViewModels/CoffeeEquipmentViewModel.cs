8803c2a9304fd97f8ea5ac162741be435287ab5a
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2022-02-14 10:18:10 +0100

    Using observable collection for example demo

--- MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
+++ MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using MvvmHelpers.Commands;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -6,10 +7,14 @@ namespace MyCoffeeApp.ViewModels
 {
     public class CoffeeEquipmentViewModel : ViewModelBase
     {
+        public ObservableCollection<string> Coffee { get; } //everytime item is added/removed/... xamarin.forms will get a notification, but you cannot add range - for every item added/removed/... there will be a notification - 100 changes means 100 notifications
+
         public CoffeeEquipmentViewModel()
         {
             IncreaseCount = new Command(OnIncrease);
             CallServerCommand = new AsyncCommand(CallServer); //without MVVM Helpers we would need to call it like: CallServerCommand = new Command(async => await CallServer()); - hack, not elegant, can swallow exceptions
+            Coffee = new ObservableCollection<string>();
+
             Title = "Coffee Equipment";
         }
 
@@ -31,6 +36,11 @@ namespace MyCoffeeApp.ViewModels
             CountDisplay = $"You clicked {count} time(s).";
         }
 
-        async Task CallServer() { }
+        async Task CallServer()
+        {
+            Coffee.Add("Yes Plz");
+            Coffee.Add("Tonx");
+            Coffee.Add("Blue Bottle");
+        }
     }
 }
