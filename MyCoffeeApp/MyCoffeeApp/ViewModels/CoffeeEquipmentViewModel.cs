a3c2dfc0451bdcb5a600b74ff6d54ce4deb80376
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2022-02-14 10:06:05 +0100

    AsyncCommand example

--- MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
+++ MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using MvvmHelpers.Commands;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace MyCoffeeApp.ViewModels
@@ -8,10 +9,12 @@ namespace MyCoffeeApp.ViewModels
         public CoffeeEquipmentViewModel()
         {
             IncreaseCount = new Command(OnIncrease);
+            CallServerCommand = new AsyncCommand(CallServer); //without MVVM Helpers we would need to call it like: CallServerCommand = new Command(async => await CallServer()); - hack, not elegant, can swallow exceptions
             Title = "Coffee Equipment";
         }
 
         public ICommand IncreaseCount { get; }
+        public ICommand CallServerCommand { get; }
 
         int count = 0;
         string countDisplay = "Click me.";
@@ -27,5 +30,7 @@ namespace MyCoffeeApp.ViewModels
             count++;
             CountDisplay = $"You clicked {count} time(s).";
         }
+
+        async Task CallServer() { }
     }
 }
