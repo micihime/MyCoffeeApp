65593e8ff587469bc8aa88b07d2b33eec694c11c
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2022-02-15 17:13:26 +0100

    Preparing Coffee viewmodel, clearing coffee page

--- MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
+++ MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MvvmHelpers;
 using MvvmHelpers.Commands;
-using System.Collections.Generic;
+using MyCoffeeApp.Models;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -8,19 +9,29 @@ namespace MyCoffeeApp.ViewModels
 {
     public class CoffeeEquipmentViewModel : ViewModelBase
     {
-        public ObservableRangeCollection<string> Coffee { get; } //everytime item is added/removed/... xamarin.forms will get a notification, but you cannot add range - for every item added/removed/... there will be a notification and an update - 100 changes means 100 notifications and updates
+        public ObservableRangeCollection<Coffee> Coffee { get; set; } //everytime item is added/removed/... xamarin.forms will get a notification, but you cannot add range - for every item added/removed/... there will be a notification and an update - 100 changes means 100 notifications and updates
+        public ObservableRangeCollection<Grouping<string, Coffee>> CoffeeGroups { get; set; }
+
+        public ICommand RefreshCommand { get; }
 
         public CoffeeEquipmentViewModel()
         {
-            IncreaseCount = new Command(OnIncrease);
-            CallServerCommand = new AsyncCommand(CallServer); //without MVVM Helpers we would need to call it like: CallServerCommand = new Command(async => await CallServer()); - hack, not elegant, can swallow exceptions
-            Coffee = new ObservableRangeCollection<string>();
-
             Title = "Coffee Equipment";
+
+            Coffee = new ObservableRangeCollection<Coffee>();
+            CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
+
+            var image = "https://upload.wikimedia.org/wikipedia/commons/c/c5/Roasted_coffee_beans.jpg";
+            Coffee.Add(new Coffee { Roaster = "R1", Name = "C1", Image = image });
+            Coffee.Add(new Coffee { Roaster = "R2", Name = "C2", Image = image });
+            Coffee.Add(new Coffee { Roaster = "R3", Name = "C3", Image = image });
+
+            CoffeeGroups.Add(new Grouping<string, Coffee>("G1", Coffee.Take(2).ToList()));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("G2", Coffee.Skip(2).Take(1).ToList()));
+
+            RefreshCommand = new AsyncCommand(Refresh); //without MVVM Helpers we would need to call it like: CallServerCommand = new Command(async => await CallServer()); - hack, not elegant, can swallow exceptions
         }
 
-        public ICommand IncreaseCount { get; }
-        public ICommand CallServerCommand { get; }
 
         int count = 0;
         string countDisplay = "Click me.";
@@ -37,10 +48,11 @@ namespace MyCoffeeApp.ViewModels
             CountDisplay = $"You clicked {count} time(s).";
         }
 
-        async Task CallServer()
+        async Task Refresh()
         {
-            var items = new List<string> { "Yes Plz", "Tonx", "Blue Bottle" };
-            Coffee.AddRange(items);
+            IsBusy = true;
+            await Task.Delay(2000);
+            IsBusy = false;
         }
     }
 }
