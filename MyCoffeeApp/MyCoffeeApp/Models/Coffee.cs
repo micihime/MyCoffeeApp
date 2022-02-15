91cbb71038f1323099266b801abc294131339e15
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2022-02-15 17:12:47 +0100

    Adding Coffee Model

---
+++ MyCoffeeApp/MyCoffeeApp/Models/Coffee.cs
@@ -0,0 +1,9 @@
+﻿namespace MyCoffeeApp.Models
+{
+    public class Coffee
+    {
+        public string Roaster { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
